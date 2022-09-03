73f0a8dfdb04cb69deec3194145e07144d2b0db0
Robert Vari <mail.robertvari@gmail.com>
2022-09-03 17:08:23 +0200

    main layout and button

--- My_Qt_Form.py
+++ My_Qt_Form.py
@@ -1,10 +1,15 @@
-from PySide2.QtWidgets import QWidget, QApplication
+from PySide2.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton
 import sys
 
 
 class RegistrationForm(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("My Registration Form")
+
+        main_layout = QVBoxLayout(self)
+        save_user_data_bttn = QPushButton("Save User Data")
 
 
 app = QApplication(sys.argv)
