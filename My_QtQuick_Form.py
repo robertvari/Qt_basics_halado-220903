3c81ef0ea909c22220a45d5db6027e34f9fba330
Robert Vari <mail.robertvari@gmail.com>
2022-09-04 15:03:04 +0200

    QtQuick window is ready

--- My_QtQuick_Form.py
+++ My_QtQuick_Form.py
@@ -3,9 +3,25 @@ import sys, os
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtGui import QGuiApplication
 
+APP_ROOT = os.path.dirname(__file__)
+MAIN_QML = os.path.join(APP_ROOT, "main.qml")
+
+
 class RegistrationForm:
     def __init__(self):
-        pass
+        # create an instance from QGuiApplication
+        self.app = QGuiApplication(sys.argv)
 
+        # instance from QtQuick engine
+        self.engine = QQmlApplicationEngine()
+
+        # load .qml file
+        self.engine.load(MAIN_QML)
+        
+        # check if there is a root object in the QML file
+        if not self.engine.rootObjects():
+            sys.exit(-1)
+        
+        sys.exit(self.app.exec_())
 
 RegistrationForm()
