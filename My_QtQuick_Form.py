1a49d3758510e9488c5b4d2a06a1f555fad59296
Robert Vari <mail.robertvari@gmail.com>
2022-09-04 14:49:55 +0200

    QtQuick Form

---
+++ My_QtQuick_Form.py
@@ -0,0 +1,11 @@
+import imp
+import sys, os
+from PySide2.QtQml import QQmlApplicationEngine
+from PySide2.QtGui import QGuiApplication
+
+class RegistrationForm:
+    def __init__(self):
+        pass
+
+
+RegistrationForm()
---
+++ main.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+
+ApplicationWindow{
+    title: "My First QtQuick Form"
+    visible: true
+    width: 500
+    height: 300
+}
